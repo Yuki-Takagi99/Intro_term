72f1566543f8b6185ee04d5a6f0fb873759bc2f6
yuki takagi <yuki99takagi@gmail.com>
2020-08-06 17:31:15 +0900

    Add : Fizz_buzz問題

---
+++ algorithm_classic/fizz_buzz.rb
@@ -0,0 +1,31 @@
+# def fizz_buzz
+#   for num in 1..100
+#     if num % 15 == 0
+#       puts "FizzBuzz"
+#     elsif num % 3 == 0
+#       puts "Fizz"
+#     elsif num % 5 == 0
+#       puts "Buzz"
+#     else
+#       puts num
+#     end
+#   end
+# end
+
+# fizz_buzz
+
+def test
+  (1..100).each do |i|
+    if i % 15 == 0
+      puts "FizzBuzz"
+    elsif i % 3 == 0
+      puts "Fizz"
+    elsif i % 5 == 0
+      puts "Buzz"
+    else
+      puts i
+    end
+  end
+end
+
+test
